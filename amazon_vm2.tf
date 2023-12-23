24f408b3d14cf7586985ec68b122933d6261ff48
dimi4 <31467280+dimi4ik@users.noreply.github.com>
2023-12-23 17:39:40 +0100

    amazon_vm2.tf small

--- amazon_vm2.tf
+++ amazon_vm2.tf
@@ -3,8 +3,8 @@
 resource "aws_instance" "my_Ubuntu" {
 # ami           = "ami-090f10efc254eaf55"
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  #tags = var.common_tags
+  #instance_type = "t3.micro"
+  tags = var.common_tags
   instance_type = "t3.small"
 }
 
