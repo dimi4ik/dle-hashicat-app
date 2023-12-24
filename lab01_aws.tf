af13a04c1a629656434d6f18ba450ae8c3e04e51
dimi4ik <dima@lejkin.de>
2023-12-24 10:37:03 +0000

    vcp_eu

--- lab01_aws.tf
+++ lab01_aws.tf
@@ -1,5 +1,5 @@
 
-/*
+
 
 #Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
@@ -23,7 +23,7 @@ resource "aws_subnet" "private_subnets" {
   for_each          = var.private_subnets
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
-   availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+#   availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
 
   tags = {
     Name      = each.key
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_subnets" {
   for_each                = var.public_subnets
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+ # availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
   map_public_ip_on_launch = true
 
   tags = {
@@ -116,4 +116,3 @@ resource "aws_nat_gateway" "nat_gateway" {
   }
 }
 
-*/
