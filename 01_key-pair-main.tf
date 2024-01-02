c4f51eb9f78fe4a0ee08f807405a589b085e4ca9
Dima Lejkin <dmitrij.lejkin@abraxas.ch>
2024-01-02 18:14:39 +0100

    key erstellen für linux und windows

--- 01_key-pair-main.tf
+++ 01_key-pair-main.tf
@@ -1,6 +1,6 @@
-#####################
-## Key Pair - Main ##
-#####################
+##############################
+## Key Pair - Main  Windows ##
+##############################
 
 # Generates a secure private key and encodes it as PEM
 resource "tls_private_key" "key_pair" {
@@ -21,9 +21,38 @@ resource "local_file" "ssh_key" {
 }
 
 
-# Output private key content
-output "ssh_key" {
-  value = tls_private_key.key_pair.private_key_pem
-  #sensitive = true
+##############################
+## Key Pair - Main  Linux   ##
+##############################
+
+resource "tls_private_key" "linux_vm" {
+  algorithm = "ED25519"
 }
 
+locals {
+  private_key_filename = "${var.prefix}-ssh-key.pem"
+}
+
+resource "aws_key_pair" "linux_vm" {
+  key_name   = local.private_key_filename
+  public_key = tls_private_key.linux_vm.public_key_openssh
+}
+
+
+##############################
+## Output - Key             ##
+##############################
+
+# Output private key content
+output "ssh_key_linux" {
+  value     = "${var.prefix}-ssh-key.pem"
+  sensitive = true
+}
+
+
+
+# Output private key content
+output "ssh_key" {
+  value     = tls_private_key.key_pair.private_key_pem
+  sensitive = true
+}
